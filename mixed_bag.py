5a22b20177a016ca9c406f6c6d48cb067560ed66
saikumar <yerra.sai@mountblue.io>
2019-06-08 13:34:06 +0530

    mixed-bag done

--- mixed_bag.py
+++ mixed_bag.py
@@ -1,23 +1,29 @@
+import random
+import os
 def unique(items):
     """
     Return a set of unique values belonging to the `items` list
     """
-    pass
+    return set(items)
 
 
 def shuffle(items):
     """
     Shuffle all items in a list
     """
+    return random.shuffle(items)
 
 
 def getcwd():
     """
     Get current working directory
     """
+    return os.getcwd()
+
 
 
 def mkdir(name):
     """
     Create a directory at the current working directory
     """
+    os.mkdir(name)
