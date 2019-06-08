70a96b4a916d136f7e2fa803e866381b16c264f9
saikumar <yerra.sai@mountblue.io>
2019-06-08 12:14:13 +0530

    dictionaries done

--- dictionaries.py
+++ dictionaries.py
@@ -1,16 +1,35 @@
+import string
+
 def word_count(s):
     """
     Find the number of occurrences of each word
     in a string(Don't consider punctuation characters)
     """
-    pass
+    y=s.split()
+    for i in y:
+        x=y.index(i)
+        print(y[x])
+        for j in string.punctuation:
+            y[x]=y[x].strip(j)
+            y[x]=y[x].strip()
+    words_count = dict()
+    for i in y:
+        words_count[i]=y.count(i)
+    return words_count
+
+
 
 
 def dict_items(d):
     """
     Return a list of all the items - (key, value) pair tuples - of the dictionary, `d`
     """
-    pass
+    # lst=[]
+    # for a,b in d:
+    #     temp=(a,b)
+    #     lst.append(temp)
+    # return lst
+    return list(d.items())
 
 
 def dict_items_sorted(d):
@@ -18,4 +37,4 @@ def dict_items_sorted(d):
     Return a list of all the items - (key, value) pair tuples - of the dictionary, `d`
     sorted by `d`'s keys
     """
-    pass
+    return list(sorted(d.items()))
